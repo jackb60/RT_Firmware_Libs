ddfbffd08943092208c22778c455ab3702a4c0d5
elleryellery <ellerymcd@gmail.com>
2025-12-12 20:47:01 -0500

    Fixed calls of writeReg

--- DRV8452.cpp
+++ DRV8452.cpp
@@ -1,15 +1,15 @@
 void DRV8452::setCurrentLimit(float current){
   _SPI->beginTransaction(_settings);
   byte value = round((current * 0.66 / 3.3) * 256) - 1;
-  writeReg(0x0E, value);
+  _writeReg(0x0E, value);
   delayMicroseconds(1);
-  writeReg(0x10, 0x12);
+  _writeReg(0x10, 0x12);
   _SPI->endTransaction();
 }
 
 void DRV8452::fullStep(bool foward){
   _SPI->beginTransaction(_settings);
-  writeReg(0x05, (forward << 7) | 0b01110000)
+  _writeReg(0x05, (forward << 7) | 0b01110000)
   _SPI->endTransaction();
 }
 
@@ -17,7 +17,7 @@ void DRV8452::setup(){
   setCurrentLimit(3.5);
   delayMicroseconds(1);
   _SPI->beginTransaction(_settings);
-  writeReg(0x05, 0b00110000);
+  _writeReg(0x05, 0b00110000);
   _SPI->endTransaction();
 }
 
